d7397dd66e2046d2d38853dd11dd894237f00fd8
zhongxia <418760129@qq.com>
2015-07-30 17:47:19 +0800

    last commit

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# bench
-test bench
+# Bench
+
+------
+
+A library to support the benchmarking of requests, similar to webbench.
+
+
+## Example usage
+
+
+### 1. simple usage 
+
+
+```node
+var bench = require('./bench');
+var option={
+      count:100,   //request times
+      method:"get",
+      url:"https://www.baidu.com/"
+  }
+bench(option);
+```
+result:
+```node
+pass 100
+pass cent 100 %
+benchmark took 1.880738569 s
+benchmark took 0.51501380586 s/op
+```
+
+### 2. what should i do if the request need cookies？ 
+
+
+
+```node
+var bench = require('./bench');
+var option={
+      count:100,   //request times
+      method:"post",
+      url:"https://www.a.com/",
+      param:{},
+      getCookieUrl:"https://www.a.com/login",
+      getCookieParam:"{user:123,pass:123}",
+      resultAssert:{dataA:1},    //check the json result
+  }
+bench(option);
+```
--- bench.js
+++ bench.js
@@ -15,30 +15,31 @@ Array.prototype.sum = function () {
     return sum
 };
 
-var bench = function (arg_count, arg_url, arg_method, arg_param, arg_resultAssert, arg_needCookie, arg_getCookieUrl, arg_getCookieParam) {
+var bench = function (arg_param) {
 
-    if (!validator.isInt(arg_count)) throw new Error("count is not a number");
-    if (!validator.isURL(arg_url))   throw new Error("wrong url");
-    if (arg_method != "post" && arg_method != "get") throw new Error("wrong method");
+
+    if (!validator.isInt(arg_param.count)) throw new Error("count is not a number");
+    if (!validator.isURL(arg_param.url))   throw new Error("wrong url");
+    if (arg_param.method != "post" && arg_param.method != "get") throw new Error("wrong method");
 
     var pass = 0;
 
-    arg_param = arg_param || {};
+    arg_param.param = arg_param.param || {};
     function httpReq(callback) {
         var time = process.hrtime();
-        var req = request[arg_method](arg_url);
-        if (arg_needCookie) agent.attachCookies(req);
-        req.send(arg_param)
+        var req = request[arg_param.method](arg_param.url);
+        if (arg_param.needCookie) agent.attachCookies(req);
+        req.send(arg_param.param)
             .end(function (err, res) {
                 //if (err) return callback(null,err);
-                if (res && arg_resultAssert && res.text && validator.isJSON(res.text)) {
+                if (res && arg_param.resultAssert && res.text && validator.isJSON(res.text)) {
                     var result = JSON.parse(res.text);
-                    for (var i in arg_resultAssert) {
-                        if (arg_resultAssert.hasOwnProperty(i) && result.hasOwnProperty(i) && result[i] === arg_resultAssert[i]) {
+                    for (var i in arg_param.resultAssert) {
+                        if (arg_param.resultAssert.hasOwnProperty(i) && result.hasOwnProperty(i) && result[i] === arg_param.resultAssert[i]) {
                             pass++;
                         }
                     }
-                } else if (!arg_resultAssert) {
+                } else if (!arg_param.resultAssert) {
                     pass++;
                 }
                 var diff = process.hrtime(time);
@@ -47,7 +48,7 @@ var bench = function (arg_count, arg_url, arg_method, arg_param, arg_resultAsser
     };
 
     var reqArray = [];
-    for (var i = 0; i < arg_count; i++) {
+    for (var i = 0; i < arg_param.count; i++) {
         reqArray.push(httpReq);
     }
 
@@ -55,10 +56,10 @@ var bench = function (arg_count, arg_url, arg_method, arg_param, arg_resultAsser
 
     async.series({
             saveCookie: function (callback) {
-                if (!arg_needCookie) return callback(null);
-                if (!arg_getCookieUrl) return callback(new Error("need cookie url"));
-                request.post(arg_getCookieUrl)
-                    .send(arg_getCookieParam)
+                if (!arg_param.needCookie) return callback(null);
+                if (!arg_param.getCookieUrl) return callback(new Error("need cookie url"));
+                request.post(arg_param.getCookieUrl)
+                    .send(arg_param.getCookieParam)
                     .end(function (err, res) {
                         if (err) {
                             callback(err);
@@ -75,9 +76,9 @@ var bench = function (arg_count, arg_url, arg_method, arg_param, arg_resultAsser
             if (err) return console.log(err);
             var diff = process.hrtime(time);
             console.log('pass %d', pass);
-            console.log('pass cent %d %', (pass / arg_count) * 100);
+            console.log('pass cent %d %', (pass / arg_param.count) * 100);
             console.log('benchmark took %d s', (diff[0] * 1e9 + diff[1]) / 1000000000);
-            console.log('benchmark took %d s/op', (results.req.sum() / arg_count / 1000000000));
+            console.log('benchmark took %d s/op', (results.req.sum() / arg_param.count / 1000000000));
         });
 
 
